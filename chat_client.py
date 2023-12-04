e82a6323b72659e6b98f4f52a34ce97196ffc287
Daltonshults <dshults@cocc.edu>
2023-12-04 12:33:52 -0800

    Added the json formatting for messages from client to server.

--- chat_client.py
+++ chat_client.py
@@ -3,17 +3,45 @@ import sys
 import json
 from chatuicurses import init_windows, read_command, print_message, end_windows
 
-def runner_1():
+def runner_1(socket):
     '''
-    Listens for incoming messages
+    Recieves packets from the server, and then displays those results
+    on screen
     '''
     ...
 
 def runner_2():
+    '''
+    Probably not needed because I can use the main thread
+    '''
     ...
 
 def usage():
     print("usage: select_client.py name host port", file=sys.stderr)
+
+def create_hello_string(name):
+    
+    hello = {
+        "type": "hello",
+        "nick": name
+    }
+
+    hello_str = json.dumps(hello)
+    hello_str_bytes = hello_str.encode()
+    
+    return hello_str_bytes    
+
+def create_message_string(command):
+
+    message = {
+        "type" : "chat",
+        "message": command
+    }
+
+    message_str = json.dumps(message)
+
+    return message_str.encode()
+
 
 def main(argv):
 
@@ -31,20 +59,21 @@ def main(argv):
     s.connect((host, port))
 
     # Create connection JSON and send it
-    hello = {
-        "type": "hello",
-        "nick": name
-    }
+    # hello = {
+    #     "type": "hello",
+    #     "nick": name
+    # }
 
-    hello_str = json.dumps(hello)
-    hello_str_bytes = hello_str.encode()
-    s.send(hello_str_bytes)
+    # hello_str = json.dumps(hello)
+    # hello_str_bytes = hello_str.encode()
+    s.send(create_hello_string(name))
 
 
     while True:
         try:
             command = read_command("Enter a thing> ")
-            command_bytes = command.encode()
+            command_bytes = create_message_string(command)
+            #print_message(command_bytes)
             s.send(command_bytes)
         except:
             break
